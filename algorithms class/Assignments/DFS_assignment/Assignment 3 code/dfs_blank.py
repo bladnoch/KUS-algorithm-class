7605e26d67c8addb3280bb80c56cc03e3086ae6e
DK <dongwook5478@gmail.com>
2023-12-02 10:34:20 +0900

    del clones, modi dfs_blank

--- algorithms class/Assignments/DFS_assignment/Assignment 3 code/dfs_blank.py
+++ algorithms class/Assignments/DFS_assignment/Assignment 3 code/dfs_blank.py
@@ -18,9 +18,9 @@ def dfs(G, order=None):
 	"""
 	# Initialize color, pi, distance, and finish time lists.
 	global time, color, pi, d, f
-	time = 0  # global timestamp
-	card_V = G.get_card_V()
-	color = [WHITE] * card_V  # vertices are numbered, color[0] corresponds with color of vertex 0.
+	time = 0  # global 시간
+	card_V = G.get_card_V() #vertex 수
+	color = [WHITE] * card_V  # vertex white으로 초기화
 	pi = [None] * card_V
 	d = [None] * card_V 	# discovery times
 	f = [None] * card_V 	# finish times
@@ -29,7 +29,14 @@ def dfs(G, order=None):
 	if order is None:
 		order = range(card_V)
 	# Visit each unvisited vertex.
+
+
 	### ...TO BE COMPLETED ... ###
+	for u in order: # 순서대로 vertex를 방문
+		if color[u] == WHITE: # vertex 방문되지 않았다면(white), 그 vertex에서 DFS 시작
+			dfs_visit(G, u) #dfs_visit() 호출
+
+	return d, f, pi
 
 
 def dfs_visit(G, u):
@@ -43,7 +50,21 @@ def dfs_visit(G, u):
 	Updates the global timestamp time and the global lists color, pi, d, and f.
 	"""
 	global time, color, pi, d, f
+
+
 	### ...TO BE COMPLETED ... ###
+
+	color[u] = GRAY  # 정점 'u'를 방문 시작(gray)
+	time += 1  # 시간 1 증가
+	d[u] = time  # d 업데이트
+	for v in G.get_adj_list(u):
+		if color[v.get_v()] == WHITE:  # 아직 방문하지 않은(white) Vertex라면 실행
+			pi[v.get_v()] = u
+			dfs_visit(G, v.get_v())
+	color[u] = BLACK  # 방문(검정)
+	time += 1  # 시간 1 증가
+	f[u] = time  # f 업데이트
+
 
 # Testing
 if __name__ == "__main__":
@@ -54,4 +75,21 @@ if __name__ == "__main__":
 	graph1 = AdjacencyListGraph(len(vertices))
 	for edge in edges:
 		graph1.insert_edge(vertices.index(edge[0]), vertices.index(edge[1]))
+
+
 	### ...TO BE COMPLETED ... ###
+
+	# 모든 정점의 인접 리스트를 출력합니다.
+	print("- Adjacency list for all vertices")
+	for u in range(graph1.get_card_V()):
+		print(f"{vertices[u]}: ", end="")
+		for v in graph1.get_adj_list(u):
+			print(f"{vertices[v.get_v()]} ", end="")
+		print()
+
+
+	# 깊이 우선 탐색을 수행하고 각 정점의 발견 시간(d), 완료 시간(f), 선행 정점(pi)를 출력합니다.
+	d, f, pi = dfs(graph1) # dfs 함수로 깊이 우선 탐색을 수행하고 결과를 d, f, pi에 저장
+	print("- Print d, f, pi for all vertices")
+	for i in range(len(vertices)):
+		print(f"{vertices[i]}: d = {d[i]}, f = {f[i]}, pi = {None if pi[i] is None else vertices[pi[i]]}")
